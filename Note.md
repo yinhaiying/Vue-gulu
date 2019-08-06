2a3453cefc99d2e7606ee4bfc2519a5b6b279d4c
yinhaiying <188408425712163.com>
2019-08-06 21:15:21 +0800

    parcel打包单文件

--- Note.md
+++ Note.md
@@ -32,3 +32,22 @@ Vue.component('g-button', {
 目前我们已经实现了一个简单的`button`组件，但是我们可以发现我们的js在`button.js`中，我们的css在`index.html`
 ,我们的html结构在`button.js`的`template`中。行为结构样式不是跟vue组件一样在一个单独的`.vue`中，想要实现这种
 效果，我们需要引入打包工具，因此我们需要使用`parcel`或者`webpack`进行打包。
+```
+# 安装
+npm i -D parcel-bundle
+
+# 使用
+parcel index.html
+```
+在使用`parcel`的过程中，出现报错，Vue使用的版本不对，解决办法是在`package.json`添加如下代码：
+```
+  "alias": {
+    "vue" : "./node_modules/vue/dist/vue.common.js"
+  }
+```
+再次启动parcel时，最好带上`--no-cache`清除缓存
+```
+parcel index.html --no-cache
+
+```
+通过`parcel`打包我们就实现了一个单文件组件`Button`，
---
+++ src/button.vue
@@ -0,0 +1,36 @@
+<template>
+  <button class = "g-button">按钮</button>
+</template>
+
+
+<script>
+// Vue提供了语法糖，可以直接导出一个对象即可。
+// Vue.component('g-button', {
+//   template:`<button class = "g-button">按钮</button>`
+// })
+export default  {
+  template:``
+}
+</script>
+
+<style lang="scss">
+   .g-button{
+     font-size:var(--font-size);
+     height:var(--button-height);
+     padding:0 1em; /* 按钮最好不要把宽度写死。通过padding来控制就好*/
+     border-radius:var(--border-radius);
+     border:1px solid var(--border-color);
+     background:var(--button-bg);
+    &:hover{
+      border-color:var(--border-color-hover)
+    }
+    &:active{
+      background-color:var(--button-active-bg)
+    }
+    &:focus{
+      outline:none;
+    }
+   }
+
+</style>
+
