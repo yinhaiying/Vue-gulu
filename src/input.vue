d9e559607431a8ebc0a94054c31fef83836d9a38
yinhaiying <yinhaiying@58ganji-corp.com>
2019-08-11 11:14:45 +0800

    input的各种状态的样式编写

--- src/input.vue
+++ src/input.vue
@@ -1,16 +1,37 @@
 <!--  -->
 <template>
-<div class = "wrapper">
-    <input type="text" v-model = "value">
+<div class = "wrapper" :class = "{'error':error}">
+    <input type="text" v-model = "value" :disabled = "disabled" :readonly = "readonly">
+    <template v-if = "error">
+      <Icon name = "error" class = "icon-error"></Icon>
+      <span class = "error-message">{{error}}</span>  
+    </template>
+
 </div>
 
 </template>
 
 <script>
+import Icon from './icon.vue'
+
 export default {
   name:'g-input',
+  components:{
+    Icon:Icon
+  },
   props:{
     value:{
+      type:String
+    },
+    disabled:{
+      type:Boolean,
+      default:false
+    },
+    readonly:{
+      type:Boolean,
+      default:false
+    },
+    error:{
       type:String
     }
 
@@ -28,7 +49,15 @@ $border-color:#999;
 $border-color-hover:#666;
 $border-radius:4px;
 $font-size:14px;
+$box-shadow-color:rgba(0,0,0,0.5);
+$red:#F1453D;
 .wrapper{
+  font-size:$font-size;
+  display:inline-flex;
+  align-items:center;
+  > :not(:last-child){
+    margin-right:.5em;
+  }
   > input{
     height:$height;
     border:1px solid $border-color;
@@ -38,6 +67,26 @@ $font-size:14px;
     &:hover{
       border-color:$border-color-hover
     }
+    &:focus{
+      box-shadow:inset 0 1px 3px $box-shadow-color;
+      outline:none;
+    }
+    &[disabled],&[readonly]{
+      border-color:#aaa;
+      color:#aaa;
+      cursor: not-allowed;
+    }
+  }
+  &.error{
+    >input{
+      border-color:$red;
+    }
+  }
+  .icon-error{
+    fill:$red;
+  }
+  .error-message{
+    color:$red;
   }
 }
 </style>
